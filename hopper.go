86ee59816e044523eff02048a6c72f7856c6d138
Kamil Chmielewski <kamil.chmielewski@allegrogroup.com>
2015-06-03 09:15:20 +0200

    First working version of hopper install

--- hopper.go
+++ hopper.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type Hop interface {
@@ -13,16 +15,53 @@ type Hop interface {
 
 type hops map[string][]Hop
 
+const (
+	appName  = "hopper"
+	hopsFile = "hop.yaml"
+)
+
 func main() {
 	cmdName := os.Args[0][strings.LastIndex(os.Args[0], "/")+1:]
-	h, err := getHop(cmdName)
+
+	if cmdName != appName {
+		runHop(cmdName, os.Args[1:])
+	}
+
+	app := kingpin.New(appName, "Docker commands in your PATH")
+
+	run := app.Command("run", "Run specified hop")
+	runName := run.Arg("hop", "Hop name to run").Required().String()
+	runArgs := run.Arg("args", "Arguments to hop").Required().Strings()
+
+	install := app.Command("install",
+		"Install hop.yaml as local commands")
+
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case run.FullCommand():
+		runHop(*runName, *runArgs)
+	case install.FullCommand():
+		localHops, err := LoadHops(hopsFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for h := range localHops {
+			if err := os.Symlink(os.Args[0], h); err == nil {
+				log.Println(h, "successfully installed")
+			} else {
+				log.Fatalf("Error while installing %v: %v", h, err)
+			}
+		}
+	}
+}
+
+func runHop(name string, args []string) {
+	h, err := getHop(name)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cmdArgs := os.Args[1:]
-
-	exitCode, err := h.Run(cmdArgs...)
+	exitCode, err := h.Run(args...)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(exitCode)
@@ -32,7 +71,7 @@ func main() {
 }
 
 func getHop(name string) (Hop, error) {
-	localHops, err := LoadHops("hop.yaml")
+	localHops, err := LoadHops(hopsFile)
 	if err != nil {
 		log.Fatal(err)
 	}
